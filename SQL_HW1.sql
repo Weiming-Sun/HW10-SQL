9e0d310f6bf9ab0a6a4d1c7dff63efee52c07051
Weiming-Sun <30918347+Weiming-Sun@users.noreply.github.com>
2018-01-27 18:26:27 -0800

    Add files via upload

--- SQL_HW1.sql
+++ SQL_HW1.sql
@@ -237,3 +237,23 @@ limit 5;
 #you would like to have an easy way of viewing the Top five genres by gross revenue. 
 #Use the solution from the problem above to create a view. 
 #If you haven't solved 7h, you can substitute another query to create a view.
+create view exec_view as
+select c.name, sum(p.amount)
+from category c
+join film_category fc 
+on c.category_id = fc.category_id
+join inventory i
+on fc.film_id = i.film_id
+join rental r
+on i.inventory_id = r.inventory_id
+join payment p
+on r.rental_id = p.rental_id
+group by c.name
+order by sum(p.amount) desc
+limit 5;
+
+#8b. How would you display the view that you created in 8a?
+select * from exec_view;
+
+#8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+drop view exec_view;
